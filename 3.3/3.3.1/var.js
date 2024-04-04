6cef121542361e4bdb4806dffefe9d0c5f80df06
jhmin99 <mjh012399@gmail.com>
2024-04-05 00:27:06 +0900

    feat: implement 3.3.1

---
+++ 3.3/3.3.1/var.js
@@ -0,0 +1,10 @@
+// const odd = 'CJS 홀수 입니다.';
+// const even = 'CJS 짝수 입니다.';
+
+// module.exports = {
+//     odd,
+//     even,
+// };
+
+exports.odd = 'CJS 홀수 입니다.';
+exports.even = 'CJS 짝수 입니다.';
