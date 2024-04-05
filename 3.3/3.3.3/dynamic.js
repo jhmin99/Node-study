32e19e8740d41acee1056008bd12f7770032d2f3
jhmin99 <mjh012399@gmail.com>
2024-04-05 11:36:38 +0900

    feat: implement 3.3.3

---
+++ 3.3/3.3.3/dynamic.js
@@ -0,0 +1,5 @@
+const a = false;
+if(a){
+    require('../3.3.1/func');
+}
+console.log('성공');
---
+++ 3.3/3.3.3/dynamic.mjs
@@ -0,0 +1,8 @@
+const a = true;
+if(a){
+    const m1 = await import ('../3.3.2/func.mjs');
+    console.log(m1);
+    const m2 = await import ('../3.3.2/var.mjs');
+    console.log(m2);
+}
+console.log('성공');
